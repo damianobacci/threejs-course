933f3afdc00db9417fb00e6e4fb7a3d6983418a3
Damiano Bacci <damiano.bacci@gmail.com>
2023-12-30 17:19:11 +0100

    Orbit controls

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
 const group = new THREE.Group();
-group.scale.y = 2;
-group.rotation.y = 0.2;
 scene.add(group);
 
 const cube1 = new THREE.Mesh(
@@ -16,14 +15,14 @@ group.add(cube1);
 
 const cube2 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 );
 cube2.position.x = 0;
 group.add(cube2);
 
 const cube3 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
 );
 cube3.position.x = 1.5;
 group.add(cube3);
@@ -34,7 +33,7 @@ const sizes = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 5;
 scene.add(camera);
 
 // const axesHelper = new THREE.AxesHelper(2);
@@ -45,4 +44,27 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const cursor = { x: 0, y: 0 };
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  renderer.render(scene, camera);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 3;
+  // camera.lookAt(group.position);
+  controls.update();
+  window.requestAnimationFrame(tick);
+};
+
+tick();
